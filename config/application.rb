65bf1942d15569d8c2c0f16f46fc6ceaff60a15c
Jason Watkins <goushoryuken@gmail.com>
2022-08-20 08:32:09 -0700

    autoload classic

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,7 @@ module ChartsApi
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.autoloader = :classic
   end
 end
